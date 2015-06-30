2c69be09552cf330111f463e8c33900cadaa6dd0
Connor <connorbfranklin@gmail.com>
2015-06-30 11:50:04 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var express = require('express');
+var app = express();
+
+var apiUrl = 'http://gsa.gov';
+
+app.use('/', function(req, res) {
+    var url = apiUrl + req.url;
+    console.log('request: ', url)
+    request({
+    	uri: url
+    }, function(error, response, html) {
+    	if (!error && response.statusCode == 200) {
+    		var $ = cheerio.load(html);
+            var pageLayout = $('#pageLayout').html();
+        	res.header('Access-Control-Allow-Origin', '*').send(pageLayout);
+    	}
+    	else{
+    		res.header('Access-Control-Allow-Origin', '*').status(404).send('404');
+    	}
+    })
+
+});
+
+app.listen(process.env.PORT || 3000);
